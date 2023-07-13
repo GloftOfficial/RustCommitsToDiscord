80458ec20b36256c327008fd64049872840d7be2
Gloft <lybetsa@live.com>
2023-07-13 12:08:20 +0300

    bleh

--- bot.py
+++ bot.py
@@ -12,46 +12,49 @@ last_commit_id = 0
 
 while True:
     # Request the page
-    response = requests.get('https://commits.facepunch.com/r/rust_reboot')
-    soup = BeautifulSoup(response.text, 'html.parser')
+    try:
+        response = requests.get('https://commits.facepunch.com/r/rust_reboot')
+        soup = BeautifulSoup(response.text, 'html.parser')
 
-    # Find the first commit
-    commit = soup.find('div', {'class' :'commit columns'})
+        # Find the first commit
+        commit = soup.find('div', {'class' :'commit columns'})
 
-    # Extract the commit id
-    commit_id = int(soup.find('div', {'class' :'commit columns'})['like-id'])
+        # Extract the commit id
+        commit_id = int(soup.find('div', {'class' :'commit columns'})['like-id'])
 
-    # If this is a new commit
-    if commit_id > last_commit_id:
-        # Update the last commit id
-        last_commit_id = commit_id
+        # If this is a new commit
+        if commit_id > last_commit_id:
+            # Update the last commit id
+            last_commit_id = commit_id
 
-        # Extract the commit details
-        author = commit.find('div', class_='author').text
-        repo = commit.find('span', class_='repo').text
-        branch = commit.find('span', class_='branch').text
-        changeset = commit.find('span', class_='changeset').text
-        commit_message = commit.find('div', class_='commits-message').text
-        avatar = commit.find('div', class_='avatar').fetchNextSiblings
-        avtr = re.search("(?P<url>https?://[^\s]+)", str(avatar)).group("url")[:-3]
+            # Extract the commit details
+            author = commit.find('div', class_='author').text
+            repo = commit.find('span', class_='repo').text
+            branch = commit.find('span', class_='branch').text
+            changeset = commit.find('span', class_='changeset').text
+            commit_message = commit.find('div', class_='commits-message').text
+            avatar = commit.find('div', class_='avatar').fetchNextSiblings
+            avtr = re.search("(?P<url>https?://[^\s]+)", str(avatar)).group("url")[:-3]
 
-        # Create the message
-        # Send the message to the Discord webhook
-        discord.post(
-            embeds=[
-                {
-                    "author": {
-                        "name": str(author),
-                        "url": "https://commits.facepunch.com/r/rust_reboot"
-                    },
-                    "title": str(repo)+str(branch)+str(changeset),
-                    "description": str(commit_message),
-                    "thumbnail": {"url": str(avtr)}
-                    
-                }
-            ],
-        )
-        # Send the message to the Discord webhook
+            # Create the message
+            # Send the message to the Discord webhook
+            discord.post(
+                embeds=[
+                    {
+                        "author": {
+                            "name": str(author),
+                            "url": "https://commits.facepunch.com/r/rust_reboot"
+                        },
+                        "title": str(repo)+str(branch)+str(changeset),
+                        "description": str(commit_message),
+                        "thumbnail": {"url": str(avtr)}
+                        
+                    }
+                ],
+            )
+            # Send the message to the Discord webhook
+    except:
+        print("error blehhhhh\n")
 
     # Wait for 50 seconds
     time.sleep(50)
